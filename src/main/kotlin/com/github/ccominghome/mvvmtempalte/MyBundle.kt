1ce4a717d60570beb7c799c5b853dfd7e0d44080
GitHub Action <action@github.com>
2021-03-30 02:27:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/ccominghome/mvvmtempalte/MyBundle.kt
+++ src/main/kotlin/com/github/ccominghome/mvvmtempalte/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ccominghome.mvvmtempalte
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ccominghome/mvvmtempalte/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ccominghome/mvvmtempalte/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ccominghome.mvvmtempalte.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ccominghome.mvvmtempalte.MyBundle
 
 class MyApplicationService {
 
